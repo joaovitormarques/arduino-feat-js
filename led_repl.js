9da62dcb7a561a64d7d12b57905afe688d2a1761
João Vitor Marques <Joo Vitor Marques>
2017-06-13 23:24:59 -0300

    add servo

--- led_repl.js
+++ led_repl.js
@@ -103,6 +103,36 @@ board.on("ready", function() {
       led11.fadeIn(1500);
       console.log('ligou 11');
       led11.fadeOut(1500, null);
+    },
+    controlServo: function(){
+      console.log("Use Up and Down arrows for CW and CCW respectively. Space to stop.");
+
+
+      //var servo = new five.Servo.Continuous(7); O servo preto não é continuous
+      process.stdin.resume();
+      process.stdin.setEncoding("utf8");
+      process.stdin.setRawMode(true);
+      process.stdin.on("keypress", function(ch, key) {
+        if (!key) {
+          return;
+        }
+
+        if (key.name === "q") {
+          console.log("Quitting");
+          process.exit();
+        } else if (key.name === "up") {
+          console.log("CW");
+          //servo.cw();
+          positivo.on();
+        } else if (key.name === "down") {
+          console.log("CCW");
+          //servo.ccw();
+          positivo.off();
+        } else if (key.name === "space") {
+          console.log("Stopping");
+          servo.stop();
+        }
+      });
     }
   });
 
